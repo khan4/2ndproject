8202d53707e8025e211af67df2c5d25a047c4115
Saad1995 <saadalishahan007@gmail.com>
2017-10-22 22:04:06 +0500

    nmjhyu

---
+++ src/Player.java
@@ -0,0 +1,24 @@
+
+public class Player {
+	private String name;
+	private int id;
+	private int rollNo;
+	 
+	
+	public void setNmae(String name){
+		this.name=name;
+	}
+	public String getName(){
+		return name;
+	}
+	
+	 
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	public int getId(){
+		return id;
+	}
+
+}
---
+++ src/SSoldier.java
@@ -0,0 +1,8 @@
+
+public class SSoldier {
+	public void main(String [] args){
+	Player player=new Player();
+			player.setNmae("qwerty");
+		
+	}
+}
